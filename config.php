17d2a314bb4b751d1e7bfa0807a3c8cfdf017764
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-05-21 21:36:02 +0330

    Update config.php
    Implementation config

--- config.php
+++ config.php
@@ -11,17 +11,62 @@
 
 
 $_config
-= array();
-
-
-
-
-///
-///
-/// Additional Config
-///
-///
-
-
-$_addsConfig
-= array();
+= array(
+    
+    
+    //
+    // Root Folder Path
+    //
+    
+    'rootFolderPath' => __DIR__,
+    
+    
+    //
+    // Libraries Folder Path
+    //
+    
+    'libsFolderPath' =>
+        '%rootFolderPath%/libs',
+    
+    
+    //
+    // Page Contents Folder Paths
+    //
+    
+    'pagesFolderPath'  =>
+        '%rootFolderPath%/pages',
+    
+    
+    //
+    // Themes
+    //
+    
+    // themes folder path
+    'themesFolderPath' =>
+        '%rootFolderPath%/themes',
+    
+    // theme name
+    'themeName' => 'muscari',
+    
+    
+    //
+    // Extention
+    //
+    
+    // use of extension in url path
+    'useOfExtension' => true,
+    
+    // default extension
+    'extensionDefault' => 'html',
+    
+    // list of valid extension
+    'extensionList' =>
+        array(
+            'html',
+            'css',
+            'js',
+            'png',
+            'jpg')
+    
+    
+    );
