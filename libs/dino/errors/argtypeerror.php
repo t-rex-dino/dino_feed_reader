628bc83a08a5fe8cd87c955ba2ee61cdb688e3b2
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-05-22 14:41:36 +0330

    Update argtypeerror.php
    added __construct metod

--- libs/dino/errors/argtypeerror.php
+++ libs/dino/errors/argtypeerror.php
@@ -7,5 +7,30 @@ namespace Dino\Errors
 {
     class ArgTypeError
         extends DevTimeError
-    {}
+    {
+        public
+        function
+        __construct(
+            $arg,
+            $argInfo = false)
+        {
+            $message
+            = '';
+            
+            if (!$argInfo == false) {
+                list($argName, $argType)
+                = explode(':', $argInf);
+                
+                $arg = var_export($arg, true);
+                
+                $message
+                = 'Argument Type Invalid. '
+                . "Type for \${$argName} "
+                . "must be [{$argType}]."
+                . "({$arg})";
+            }
+            
+            parent::__construct($message);
+        }
+    }
 }
