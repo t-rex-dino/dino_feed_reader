2e047f1665f4d1c9aed695ba69b233362799fa23
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-05-25 06:47:17 +0330

    Update emptyargerror.php
    implementation __constract method.

--- libs/dino/errors/emptyargerror.php
+++ libs/dino/errors/emptyargerror.php
@@ -2,3 +2,27 @@
 
 
 
+
+namespace Dino\Errors
+{
+    class EmptyArgError
+        extends DevTimeError
+    {
+        public
+        function
+        __construct(
+            $argName = false)
+        {
+            $message
+            = '';
+            
+            if ($argName !== false) {
+                $message
+                = 'Argument is Empty. '
+                . "{$argNamr} can't be empty.";
+            }
+            
+            parent::__construct($message);
+        }
+    }
+}
