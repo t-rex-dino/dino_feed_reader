c1bb380acd480b204dc916749383a4bb9b62971a
mim II <133511261+mim-II@users.noreply.github.com>
2023-05-30 06:49:46 +0330

    Update loader.php
    create Loader Class

--- libs/dino/general/loader.php
+++ libs/dino/general/loader.php
@@ -2,3 +2,56 @@
 
 
 
+
+namespace Dino\General
+{
+    class Loader
+    {
+        public
+        static
+        function
+        loadClass(
+            $className)
+        {
+            if (class_exists($className, false)
+             || interface_exists($className, false)) {
+                
+                return;
+            }
+            
+            self::loadFile("{$className}.php");
+            if (!class_exists($className, false)
+             && interface_exists($className, false)) {
+                
+                throw
+                new ClassNotFoundError(
+                        $className);
+            }
+        }
+        
+        
+        public
+        static
+        function
+        loadFile(
+            $path,
+            $once = false)
+        {
+            if (File::check(
+                    $path,
+                    $fullPath)) {
+                
+                if ($once) {
+                    return require_once $fullPath;
+                }
+                else {
+                    return require $fullPath;
+                }
+            }
+            
+            throw
+            new FileNotFoundError(
+                    $path);
+        }
+    }
+}
