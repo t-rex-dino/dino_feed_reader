7911a40a5ea96f2f2e5902708abe1947f058d520
Eraj <eraj@email.ir>
2023-05-31 22:25:22 +0430

    Add html content in the categories.html.php file for testing purpose

--- pages/views/categories.html.php
+++ pages/views/categories.html.php
@@ -1,1 +1,9 @@
-<div></div>
+<!-- This content is for testing purposes only -->
+<div>
+    <h1>Categories</h1>
+    <ul>
+        <li>first</li>
+        <li>second</li>
+        <li>third</li>
+    </ul>
+</div>
