301388e3013d0d3553537ea4c7a007c2f3abb374
Eraj <eraj@email.ir>
2023-06-01 17:18:48 +0430

    I wrote a function to add values & make some changes

--- pages/views/view.php
+++ pages/views/view.php
@@ -4,8 +4,26 @@ namespace pages\views;
 class view
 {
 
-    public function __construct (?string $view , string $value = '') {
-        /* Load a view */
+    /**
+     * To hold values
+     *
+     * @var array
+     */
+    private array $values = [];
+
+    public function __construct (?string $view , ?string $value = null) {
+        $this->load_view ($view);
+
+        $this->add_value ($value);
+    }
+
+    /**
+     * Loading a view
+     *
+     * @param string $view
+     * @return void
+     */
+    private function load_view (string $view): void {
         if (file_exists ("./$view")) {
             include_once "./$view";
         } else {
@@ -14,11 +32,23 @@ class view
         }
     }
 
+    /**
+     * Add a value to the $values property
+     *
+     * @param string|null $value
+     * @return void
+     */
+    private function add_value (?string $value): void {
+        if ($value) {
+            array_push ($this->values , $value);
+        }
+    }
+
     public function __set (string $property , $value): void {
         $this->$property = $value;
     }
 
-    public function __get (string $property): mixed {
+    public function __get (string $property) {
         return $this->$property;
     }
 
