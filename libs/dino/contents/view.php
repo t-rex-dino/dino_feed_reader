5bbe1b04f9a64fa9bd31132b34bc12b4f80de28f
Eraj <eraj@email.ir>
2023-06-01 18:06:08 +0430

    Editing data types in the view.php class & transfer it to correct location

--- libs/dino/contents/view.php
+++ libs/dino/contents/view.php
@@ -1,10 +1,53 @@
-<?PHP
+<?php
+namespace libs\dino\contents;
 
-
-
-
-namespace Dino\Contents
+class view
 {
-    class View
-    {}
+
+    /**
+     * To hold values
+     *
+     * @var type
+     */
+    private $values = [];
+
+    public function __construct ($view , $value = null) {
+        $this->load_view ($view);
+
+        $this->add_value ($value);
+    }
+
+    /**
+     * Loading a view
+     *
+     * @param type $view
+     */
+    private function load_view ($view) {
+        if (file_exists ("./$view")) {
+            include_once "./$view";
+        } else {
+            /* Not found view */
+            include_once './view-not-found.html';
+        }
+    }
+
+    /**
+     * Add a value to the $values property
+     *
+     * @param type $value
+     */
+    private function add_value ($value) {
+        if ($value) {
+            array_push ($this->values , $value);
+        }
+    }
+
+    public function __set ($property , $value) {
+        $this->$property = $value;
+    }
+
+    public function __get ($property) {
+        return $this->$property;
+    }
+
 }
