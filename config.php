b0c28eca14986a7eec406aca3b5228d5f9a75af5
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-06-08 23:05:07 +0330

    update config.php
    set defaultPage to news.html

--- config.php
+++ config.php
@@ -1,236 +1,58 @@
-<?PHP
+<?php
 
 
 
 
-namespace Dino\General
-{
-    use Dino\Errors\ArgTypeError;
-    use Dino\Errors\EmptyArgError;
-    use Dino\Errors\KeyNotFoundError;
+///
+///
+/// System Configuration
+///
+///
+
+
+$_config
+= array(
+    'webApp' =>
+        array(
+            'subFolder' => 'dino_feed_reader-main'),
     
-    
-    class Config
-    {
-        private
-        static
-        $_data
-        = array();
-        
-        
-        public
-        static
-        function
-        set(
-            $path,
-            $value   = null,
-            &$source = false)
-        {
-            if (is_array($path)
-             && is_null($value)
-             && empty(self::$_data)) {
-                
-                $path
-                = array_change_key_case(
-                    $path,
-                    CASE_LOWER);
-                
-                self::$_data
-                = $path;
-                
-                return;
-            }
-            
-            if (!is_string($path)) {
-                throw
-                new ArgTypeError(
-                        $path,
-                        'path:string');
-            }
-            
-            if (empty($path)) {
-                throw
-                new emptyArgError('path');
-            }
-            
-            $path
-            = strtolower($path);
-            
-            $path
-            = explode('.', $path);
-            
-            $name
-            = array_shift($path);
-            
-            if (is_array($value)) {
-                $value
-                = array_change_key_case(
-                    $value,
-                    CASE_LOWER);
-            }
-            
-            if (!empty($path)) {
-                $path
-                = array_reverse($path);
-                
-                foreach ($path as $key) {
-                    $value
-                    = array($key => $value);
-                }
-            }
-            
-            if (!is_array($source)) {
-                $source
-                =& self::$_data;
-            }
-            
-            if (!isset($source[$name])
-             || !is_array($source[$name])
-             || !is_array($value)) {
-                
-                $source[$name]
-                = $value;
-            }
-            else {
-                foreach ($value as $k => $v) {
-                    self::set($k, $v, $source[$name]);
-                }
-            }
-        }
-        
-        
-        public
-        static
-        function
-        load(
-            $configFilePath)
-        {
-            if (!is_string($configFilePath)) {
-                throw
-                new ArgTypeError(
-                        $configFilePath,
-                        'configFilePath:string');
-            }
-            
-            if (!File::check(
-                    $configFilePath,
-                    $path)) {
-                
-                throw
-                new FileNotFoundError(
-                        $configFilePath);
-            }
-            
-            require $path;
-            
-            if (!isset($_config)) {
-                throw
-                new ConfigNotFoundError(
-                        $configFilePath);
-            }
-            
-            $_config
-            = array_change_key_case(
-                $_config,
-                CASE_LOWER);
-            
-            self::$_data
-            = array_merge(
-                self::$_data,
-                $_config);
-        }
-        
-        
-        
-        public
-        static
-        function
-        check(
-            $path,
-            &$value  = false)
-        {
-            if (!is_string($path)) {
-                throw
-                new ArgTypeError(
-                        $path,
-                        'path:string');
-            }
-            
-            if (empty($path)) {
-                throw
-                new EmptyArgError(
-                        'path');
-            }
-            
-            $path
-            = strtolower($path);
-            
-            $path
-            = explode('.', $path);
-            
-            $name
-            = array_shift($path);
-            
-            if (!isset(self::$_data[$name])) {
-                return false;
-            }
-            
-            $value
-            = self::$_data[$name];
-            
-            if (!empty($path)) {
-                foreach ($path as $name) {
-                    if (!is_array($value)) {
-                        $value = false;
-                        return false;
+    'page' =>
+        array(
+            'path' =>
+                function () {
+                    $request
+                    = '';
+                    
+                    if (isset($_SERVER['REQUEST_URI'])
+                     && !empty($_SERVER['REQUEST_URI'])) {
+                        
+                        $request
+                        = strtolower(
+                            trim(
+                                $_SERVER['REQUEST_URI'],
+                                '/'));
+                        
+                        $webAppSubFolder
+                        = strtolower(
+                            trim(
+                                Dino\General\Config::get(
+                                    'WebApp.SubFolder'),
+                                '/'));
+                        
+                        $request
+                        = trim(
+                            str_ireplace(
+                                $webAppSubFolder,
+                                '',
+                                $request),
+                            '/');
                     }
                     
-                    $value
-                    = array_change_key_case(
-                        $value,
-                        CASE_LOWER);
-                    
-                    if (!isset($value[$name])) {
-                        $value = false;
-                        return false;
-                    }
-                    
-                    $value
-                    = $value[$name];
-                }
-            }
+                    return
+                    $request;
+                },
             
-            return true;
-        }
-        
-        
-        
-        public
-        static
-        function
-        get($path)
-        {
-            $args
-            = func_get_args();
-            
-            $path
-            = array_shift($args);
-            
-            if(self::check($path, $value)) {
-                if (is_callable($value)) {
-                    $value
-                    = call_user_func_array(
-                        $value,
-                        $args);
-                }
-                
-                return $value;
-            }
-            
-            throw
-            new KeyNotFoundError(
-                    'config',
-                    $path);
-        }
-    }
-}
+            'defaultPage' => 'news',
+            'defaultExt'  => 'html',
+            'useOfExt'    => true,
+            'pagesFolderPath' => 'pages'));
