8dde5975a0d7aa257733e5f992c53cd3df4f3074
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-06 19:54:16 -0700

    fix bug folder class

--- libs/dino/general/folder.php
+++ libs/dino/general/folder.php
@@ -11,7 +11,7 @@ namespace Dino\General
         function
         check(
             $path,
-            &$fullPath)
+            &$fullPath = false)
         {
             if (self::checkPath(
                         $path,
