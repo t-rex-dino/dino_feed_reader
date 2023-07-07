c5147e416190c5c5eb7f777abc97561cfcc4a732
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-07 12:54:01 +0330

    create routers class

--- libs/dino/contents/routers.php
+++ libs/dino/contents/routers.php
@@ -5,5 +5,181 @@
 namespace Dino\Contents
 {
     class Routers
-    {}
+    {
+        private
+        static
+        $_routers
+        = array();
+
+
+        private
+        static
+        $_routersFolderPath
+        = '';
+        
+
+        public
+        static
+        function
+        checkPath(
+            $router,
+            $path)
+        {
+            $router
+            = strtolower($router);
+
+            if (!is_string($path)) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router])) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router]['checkpath'])) {
+                #ERR
+            }
+
+            if (!is_callable(self::$_routers[$router]['checkpath'])) {
+                #ERR
+            }
+
+            $check
+            = call_user_func(
+                self::$_routers[$router]['checkpath'],
+                $path);
+            
+            if (!is_bool($check)) {
+                #ERR
+            }
+
+            return $check;
+        }
+        
+
+        public
+        static
+        function
+        pathToRoute(
+            $router,
+            $path)
+        {
+            $router
+            = strtolower($router);
+
+            if (!is_string($path)) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router])) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router]['topath'])) {
+                #ERR
+            }
+
+            if (!is_callable(self::$_routers[$router]['topath'])) {
+                #ERR
+            }
+
+            $route
+            = call_user_func(
+                self::$_routers[$router]['topath'],
+                $path);
+            
+            if (!is_array($route)) {
+                #ERR
+            }
+
+            return $route;
+        }
+
+
+        public
+        static
+        function
+        checkRoute(
+            $router,
+            $route)
+        {
+            $router
+            = strtolower($router);
+
+            if (!is_array($route)) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router])) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router]['checkroute'])) {
+                #ERR
+            }
+
+            if (!is_callable(self::$_routers[$router]['checkroute'])) {
+                #ERR
+            }
+
+            $check
+            = call_user_func(
+                self::$_routers[$router]['checkroute'],
+                $route);
+            
+            if (!is_bool($check)) {
+                #ERR
+            }
+
+            return $check;
+        }
+
+
+        public
+        static
+        function
+        routeToPath($router, $route)
+        {
+            $router
+            = strtolower($router);
+
+            if (!is_array($route)) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router])) {
+                #ERR
+            }
+
+            if (!isset(self::$_routers[$router]['toroute'])) {
+                #ERR
+            }
+
+            if (!is_callable(self::$_routers[$router]['toroute'])) {
+                #ERR
+            }
+
+            $path
+            = call_user_func(
+                self::$_routers[$router]['toroute'],
+                $route);
+            
+            if (!is_string($path)) {
+                #ERR
+            }
+
+            return $path;
+        }
+
+
+        public
+        static
+        function
+        setRouters($routers)
+        {
+            if (!is_array($routers)) {
+                #ERR
+            }
+        }
+    }
 }
