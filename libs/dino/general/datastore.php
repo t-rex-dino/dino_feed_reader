bdbda670c8f8728b7df9dc29df53d02055be94aa
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-07 07:49:33 +0330

    add & create DataStore class

--- libs/dino/general/datastore.php
+++ libs/dino/general/datastore.php
@@ -5,5 +5,56 @@
 namespace Dino\General
 {
     class DataStore
-    {}
+    {
+        private
+        static
+        $_store
+        = array();
+
+
+        public
+        static
+        function
+        set(
+            $path,
+            $value)
+        {
+            return
+            AryByPath::add(
+                self::$_store,
+                $path,
+                $value);
+        }
+
+
+        public
+        static
+        function
+        check(
+            $path,
+            &$value = false)
+        {
+            return
+            AryByPath::check(
+                self::$_store,
+                $path,
+                $value);
+        }
+
+
+        public
+        static
+        function
+        get($path)
+        {
+            if (self::check(
+                        $path,
+                        $value)) {
+                
+                return $value;
+            }
+
+            #ERR
+        }
+    }
 }
