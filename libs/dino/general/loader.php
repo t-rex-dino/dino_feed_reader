881874fed441346f0109e5202b43a35ac40c25bd
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-06 20:10:19 -0700

    create and coding loader class

---
+++ libs/dino/general/loader.php
@@ -0,0 +1,54 @@
+<?PHP
+
+
+
+namespace Dino\General
+{
+    class Loader
+    {
+        public
+        static
+        function
+        loadClass($className)
+        {
+            if (!is_string($className)) {
+                #ERR
+            }
+
+            if (class_exists($className, false)
+             || interface_exists($className, false)) {
+
+                return;
+            }
+
+            self::loadFile("{$className}.php");
+
+            if (class_exists($className, false)
+             && interface_exists($className, false)) {
+
+                #ERR
+            }
+        }
+
+
+        public
+        static
+        function
+        loadFile(
+            $filePath,
+            $once = false)
+        {
+            if (File::check($filePath, $fullPath)) {
+
+                if ($once) {
+                    return require_once $fullPath;
+                }
+                else {
+                    return require $fullPath;
+                }
+            }
+
+            #ERR
+        }
+    }
+}
