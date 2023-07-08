5268cb5b6d26f748cfb9e06cd6b598268db23884
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-07-07 20:18:13 -0700

    add Launchers class

---
+++ libs/dino/contents/launchers.php
@@ -0,0 +1,235 @@
+<?PHP
+
+
+
+namespace Dino\Contents
+{
+    class Launchers
+    {
+        private
+        static
+        $_launchers
+        = array();
+        
+        
+        public
+        static
+        $_launchersFolderPath
+        = 'launchers';
+        
+        
+        public
+        static
+        function
+        __callStatic(
+            $method,
+            $args)
+        {
+            $method
+            = strtolower($methed;
+            )
+            if (preg_match(
+                    '/^([a-z]+[a-z0-9]*)+_[a-z0-9]+$/i',
+                    $method)) {
+                
+                return false;
+            }
+            
+            list($launcher, $method)
+            = explode(
+                '_',
+                $method,
+                2);
+            
+            switch ($method)
+            {
+                case 'loader':
+                    $args
+                    = array_shift($args);
+                    
+                    self::addLoader(
+                            $launcher,
+                            $args)
+                    break;
+                
+                case 'routetopath':
+                    break;
+                
+                case 'check':
+                    break
+                
+                default:
+                    #ERR
+                    break;
+            }
+        }
+        
+        
+        public
+        function
+        load($router)
+        {
+            if (!self::check($route)) {
+                #ERR
+            }
+            
+            return
+            call_user_func(
+                self::$_launchers[$route['launcher']]['loader'],
+                $route);
+        }
+        
+        
+        public
+        static
+        function
+        RouteToPath($route)
+        {
+            if (!self::check($route)) {
+                #ERR
+            }
+            
+            $path
+            = call_user_func(
+                self::$_launchers[$route['launcher']]['topath'],
+                $route);
+            
+            if (!is_string($path)) {
+                #ERR
+            }
+            
+            return $path;
+        }
+        
+        
+        public
+        static
+        function
+        check($route)
+        {
+            if (!is_array($route)) {
+                #ERR
+            }
+            
+            if (!isset($route['lancher'])) {
+                #ERR
+            }
+            
+            if (!isset(self::$_launchers[$route['launcher']])) {
+                self::loadLauncherFile($route['launcher']);
+                
+                if (!isset(self::$_launchers[$route['launcher']])) {
+                    #ERR
+                }
+            }
+            
+            if (!isset(self::$_launchers[$route['launcher']]['check'])) {
+                #ERR
+            }
+            
+            $check
+            = call_user_func(
+                self::$_launchers[$route['launcher']]['check'],
+                $route);
+            
+            if (!is_bool($check)) {
+                #ERR
+            }
+            
+            return $check;
+        }
+        
+        
+        public
+        static
+        function
+        loadLauncherFile($name)
+        {
+            if (!is_string($name)) {
+                #ERR
+            }
+            
+            if (File::check(
+                        Folder::branch(
+                                    self::$_launchersFolderPath,
+                                    "{$name}.php"),
+                        $fullPath)) {
+                
+                require $fullPath;
+                
+                return true;
+            }
+            
+            return false;
+        }
+        
+        
+        public
+        static
+        function
+        addLoader(
+            $name,
+            $launcher)
+        {
+            if (!is_string($name)) {
+                #ERR
+            }
+            
+            if (!is_callable($launcher)) {
+                #ERR
+            }
+            
+            $name
+            = strtolower($name);
+            
+            self::$_launchers[$name]['loader']
+            = $launcher;
+        }
+        
+        
+        public
+        static
+        function
+        addRouteToPath(
+            $name,
+            $toPath)
+        {
+            if (!is_string($name)) {
+                #ERR
+            }
+            
+            if (!is_callable($toPath)) {
+                #ERR
+            }
+            
+            $name
+            = strtolower($name);
+            
+            self::$_launchers[$name]['topath']
+            = $toPath;
+        }
+        
+        
+        public
+        static
+        function
+        addCheckRoute(
+            $name,
+            $checker)
+        {
+            if (!is_string($name)) {
+                #ERR
+            }
+            
+            if (!is_callable($checker)) {
+                #ERR
+            }
+            
+            $name
+            = strtolower($name);
+            
+            self::$_launchers[$name]['check']
+            = $checker;
+        }
+    }
+}
