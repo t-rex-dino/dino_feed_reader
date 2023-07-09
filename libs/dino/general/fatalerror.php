a110f2f48ffd4f75bd453c0992cd207cbbd3222d
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-09 08:56:11 +0330

    add invalidArgType method

--- libs/dino/general/fatalerror.php
+++ libs/dino/general/fatalerror.php
@@ -9,6 +9,28 @@ namespace Dino\General
         public
         static
         function
+        invalidArgType(
+            $method,
+            $argName,
+            $argType)
+        {
+            $message
+            = 'Invalid Argument Type Error.'
+            . "\n For [Method: {$method}] in [Argument: {$argName}]"
+            . "acceptable [Types : {$argType}]";
+
+            $code
+            = 2111; // devTime.coding.argument.type
+
+            self::send(
+                $message,
+                $code);
+        }
+
+
+        public
+        static
+        function
         send(
             $message,
             $code)
