f52f0032ec998753566c50cc63431962685c4de8
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-16 01:38:56 +0330

    add dataStore Config

--- config.php
+++ config.php
@@ -2,4 +2,20 @@
 
 
 
-#Dino\General\Config::set();
+Dino\General\DataStore::set(
+    'config',
+    array(
+        'webApp' =>
+            array(
+                'path' =>
+                    function () {
+                        return 'users/login.html';
+                    },
+                
+                'routers' =>
+                    array(
+                        'page',
+                        'component',
+                        'res',
+                        'home',
+                        'pageNotFound'))));
