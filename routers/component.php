7c602db19cc065c697eb824e48901d10f52a1541
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-16 17:51:54 +0330

    add router component

---
+++ routers/component.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+Dino\Contents\Routers::Component_checkPath(
+    function ($path) {
+        if (!empty($path)
+         && preg_match(
+                '/^(component\/)([a-z0-9_]+'
+                . '\/)*[a-z0-9_]+(\-[a-z'
+                . '0-9_])*$/i',
+                $path)) {
+            
+            return true;
+        }
+
+        return false;
+    });
+
+
+Dino\Contents\Routers::Component_PathToRoute(
+    function ($path) {
+        $route
+        = array(
+            'launcher' => 'component');
+    
+        $path
+        = str_ireplace(
+            'componen/',
+            '',
+            $path);
+
+        if (preg_match(
+                '/^[a-z0-9_\/]+(\-[a-z0-9_])+$/',
+                $path)) {
+            
+            list($path, $route['params'])
+            = explode(
+                '-',
+                $path,
+                2);
+        }
+
+        $route['content']
+        = $path;
+
+        return $route;
+    });
