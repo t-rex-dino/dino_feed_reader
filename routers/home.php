b84c2eb67eeebfdba1c8d44e0b51668a2e9d440b
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-16 17:52:17 +0330

    add router home

---
+++ routers/home.php
@@ -0,0 +1,72 @@
+<?php
+
+
+
+Dino\Contents\Routers::Home_checkPath(
+    function ($path) {
+        if (empty($path)
+         && (Dino\General\DataStore::check(
+                'Config.WebApp.HomePath')
+             || Dino\General\DataStore::check(
+                    'Config.WebApp.HomeRoute'))) {
+            
+            return true;
+        }
+
+        return false;
+    });
+
+
+Dino\Contents\Routers::Home_PathToRoute(
+    function ($path) {
+        $route
+        = array();
+
+        if (Dino\General\DataStore::check(
+                'Config.WebApp.HomePath',
+                $path)) {
+
+            if (preg_match(
+                '/^[^\.]+\.[^\.]+$/i',
+                $path)) {
+            
+                list($path,$route['ext'])
+                = explode(
+                    '.',
+                    $path,
+                    2);
+            }
+
+            if (perg_match(
+                    '/^page\//i',
+                    $path)) {
+                
+                $path
+                = str_ireplace(
+                    'page/',
+                    '',
+                    $path);
+            }
+        
+            if (preg_match('',$path)) {
+                list($path,$route['params'])
+                = explode(
+                    '-',
+                    $path,
+                    2);
+            }
+
+            $route['content']
+            = $path;
+        }
+
+        if (empty($route)) {
+            $route
+            = Dino\General\DataStore::get('Config.WebApp.HomeRoute');
+        }
+
+        $route['launcher']
+        = 'page';
+
+        return $route;
+    });
