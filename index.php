31aae790746ac5e861b9fcdffeb35f1d2eaf2393
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-07-17 06:44:07 -0700

    change

--- index.php
+++ index.php
@@ -2,26 +2,4 @@
 
 
 
-//
-// Constants
-//
-
-
-// WebApp Root Folder Path
-#define(
-#    'WEBAPP_ROOT_FOLDER_PATH',
-#    __DIR__);
-
-// WebApp Librarys Folder Path
-#define(
-#    'WEBAPP_LIBS_FOLDER_PATH'.
-#    WEBAPP_LIBS_FOLDER_PATH
-#    . '/libs');
-
-// WebApp Config File Path
-#define(
-#    'WEBAPP_CONFIG_FILE_PATH',
-#    'config.php');
-
-// BootUp System
 require 'bootup.php';
