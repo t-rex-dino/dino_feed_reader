2fa91e50a61ce189fd881bc8b95e482d44de9391
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-07-18 07:16:53 -0700

    add methods

--- launchers/page.php
+++ launchers/page.php
@@ -1,1 +1,20 @@
 <?php
+
+
+
+Dino\Contents\Launchers::page_load(
+    function ($route) {
+        
+    });
+
+
+Dino\Contents\Launchers::page_checkRoute(
+    function ($route) {
+        
+    });
+
+
+Dino\Contents\Launchers::page_routeToPath(
+    function ($route) {
+        
+    });
