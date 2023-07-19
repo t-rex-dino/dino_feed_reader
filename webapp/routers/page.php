3f4f21e7710a77e8c08c07dcef8173d1c0c66e82
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-07-18 20:43:56 -0700

    move to webapp

--- webapp/routers/page.php
+++ webapp/routers/page.php
@@ -1,1 +1,53 @@
 <?php
+
+
+
+Dino\Contents\Routers::page_checkPath(
+    function ($path) {
+        if (!empty($path)
+         && preg_match(
+                '/^(page\/)?([a-z0-9_]+'
+                . '\/)*[a-z0-9_]+(\-[a-z'
+                . '0-9_])*$/i',
+                $path)) {
+            
+            return true;
+        }
+
+        return false;
+    });
+
+
+Dino\Contents\Routers::page_PathToRoute(
+    function ($path) {
+        $route
+        = array(
+            'launcher' => 'page');
+
+        if (preg_match(
+                '/^page\//i',
+                $path)) {
+            
+            $path
+            = str_ireplace(
+                'page/',
+                '',
+                $path);
+        }
+
+        if (preg_match(
+                '/^[a-z0-9_\/]+(\-[a-z0-9_])+$/',
+                $path)) {
+            
+            list($path, $route['params'])
+            = explode(
+                '-',
+                $path,
+                2);
+        }
+
+        $route['content']
+        = $path;
+
+        return $route;
+    });
