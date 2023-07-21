8eef16a7e2f0307c4ffa081ce2052bd281228647
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-21 23:46:25 +0330

    add method __construct & load

--- libs/dino/contents/component.php
+++ libs/dino/contents/component.php
@@ -6,7 +6,23 @@ namespace Dino\Contents
 {
     use Dino\General\VAndM;
 
-    
+
     class Component
-    {}
+        extends VAndM
+    {
+        public
+        function
+        __construct($page)
+        {
+            parent::__construct();
+        }
+
+
+        public
+        function
+        load()
+        {
+            
+        }
+    }
 }
