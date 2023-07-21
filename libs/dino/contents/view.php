89704145ae52d78880e1f412de06f14a7ab73c4f
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-07-21 05:03:18 -0700

    add new file & class

---
+++ libs/dino/contents/view.php
@@ -0,0 +1,20 @@
+<?PHP
+
+
+
+namespace Dino\Contents
+{
+    use Dino\General\VAndM;
+    
+    
+    class View
+        extends VAndM
+    {
+        public
+        function
+        load()
+        {
+            echo __FILE__;
+        }
+    }
+}
