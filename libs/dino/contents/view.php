760f013b135d8616b1a43ac28bfe84bc2df4d708
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-07-21 22:40:45 +0330

    add method __construct

--- libs/dino/contents/view.php
+++ libs/dino/contents/view.php
@@ -4,12 +4,53 @@
 
 namespace Dino\Contents
 {
+    use Dino\General\FatalError;
     use Dino\General\VAndM;
     
     
     class View
         extends VAndM
     {
+        private
+        $_viewFilePath;
+
+
+        public
+        function
+        __construct(
+            $params,
+            $viewFilePath = false)
+        {
+            if(is_string($params)
+             && $viewFilePath == false) {
+                $viewFilePath
+                = $params;
+
+                $params
+                = array();
+            }
+
+            if (!is_array($params)) {
+                FatalError::invalidArgType(
+                    __METHOD__,
+                    'params',
+                    'array');
+            }
+
+            if (!is_string($viewFilePath)) {
+                FatalError::invalidArgType(
+                    __METHOD__,
+                    'viewFilePath',
+                    'string');
+            }
+
+            parent::__construct($params);
+
+            $this->_viewFilePath
+            = $viewFilePath;
+        }
+
+
         public
         function
         load()
