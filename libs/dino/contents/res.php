ba328298f2ea5238f896aeb67265116ba017f6f1
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-07-22 19:31:39 -0700

    add filev& class

---
+++ libs/dino/contents/res.php
@@ -0,0 +1,18 @@
+<?PHP
+
+
+
+namespace Dino\Contents
+{
+    use Dino\General\VAndM;
+    
+    
+    class Res
+        extends VAndM
+    {
+        public
+        function
+        load()
+        {}
+    }
+}
