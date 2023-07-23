417968aa2f555bd6b76f459030e6ee441216735d
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-07-23 12:15:11 -0700

    coding

--- libs/dino/contents/res.php
+++ libs/dino/contents/res.php
@@ -4,6 +4,7 @@
 
 namespace Dino\Contents
 {
+    use Dino\General\Folder;
     use Dino\General\VAndM;
     
     
@@ -12,7 +13,68 @@ namespace Dino\Contents
     {
         public
         function
+        __construct($resVars)
+        {
+            if (is_string($resVars)) {
+                $resVars
+                = array(
+                    'path' => $resVars);
+            }
+            
+            parent::__construct($resVars);
+        }
+        
+        
+        public
+        function
+        __get($path)
+        {
+            if (!isset($this->$path)) {
+                $prpt
+                = strtolower($path);
+                
+                switch ($prpt)
+                {
+                    case 'resfilepath':
+                        return
+                        str_ireplace(
+                            array(
+                                $this->themeFolderPathm
+                                $this->extension,
+                                $this->resFileName),
+                            array(
+                                'Theme',
+                                'Extension',
+                                'ResFileName'),
+                            $this->resFilePathPattern);
+                        
+                        break;
+                    
+                    case 'resfilepathpattern':
+                        return
+                        DataStore::get('Config.WebApp.ResFilePathPattern');
+                        break;
+                    
+                    case 'themefolderpath':
+                        break;
+                    
+                    case 'extension':
+                        break;
+                    
+                    case 'resfilename':
+                        break;
+                }
+            }
+            
+            return
+            parent::__get($path);
+        }
+        
+        public
+        function
         load()
-        {}
+        {
+            require $this->resFilePath;
+        }
     }
 }
