208e829d375147eebb7a2ab8e56dfef1122ba7b7
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-08-13 23:43:46 +0330

    add config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+///
+///
+/// WebApp Configs
+///
+///
+
+Dino\Contents\WebApp::Config(
+    array(
+        'homePath' => '',
+        'routers' => array(),
+        'launchers' => array(),
+        'theme' => ''));
