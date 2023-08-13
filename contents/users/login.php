b41612bf98f86806644315aadbe22e4c393be158
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-08-13 23:21:21 +0330

    RESET 05222320

--- contents/users/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-$this->onLoad(
-    function () {
-        $this->view->name
-        = 'amin';
-    });
--- webapp/themes/bluebill/html/users/login.php
+++
@@ -1,5 +0,0 @@
-<section>
-    <div id="users-login">
-        <h1>Users Login</h1>
-    </div>
-</section>
