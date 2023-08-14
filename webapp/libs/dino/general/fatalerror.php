1401ee4287560cef897364f889c2981b63c2329b
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-08-14 17:47:35 +0330

    !A@Ms

--- webapp/libs/dino/general/fatalerror.php
+++ webapp/libs/dino/general/fatalerror.php
@@ -5,5 +5,25 @@
 namespace Dino\General
 {
     class FatalError
-    {}
+    {
+        public
+        static
+        function
+        invalidArgType()
+        {}
+
+
+        public
+        static
+        function
+        routersNotFound()
+        {}
+
+
+        public
+        static
+        function
+        readOnly()
+        {}
+    }
 }
