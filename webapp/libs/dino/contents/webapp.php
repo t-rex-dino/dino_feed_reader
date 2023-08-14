f4d570b69a975acd63bb21d7df88c7dc53ecc807
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-08-14 10:04:06 +0330

    complate METHOD WebApp::config

--- webapp/libs/dino/contents/webapp.php
+++ webapp/libs/dino/contents/webapp.php
@@ -4,6 +4,9 @@
 
 namespace Dino\Contents
 {
+    use Dino\General\FatalError;
+
+
     class WebApp
     {
         private
@@ -15,6 +18,35 @@ namespace Dino\Contents
         static
         function
         config($config)
-        {}
+        {
+            if (is_array($config)) {
+                if (!empty(self::$_config)) {
+                    FatalError::readOnly(
+                        __METHOD__,
+                        'WebApp.config');
+                }
+
+                self::$_config
+                = array_change_key_case(
+                    $config);
+            }
+
+            if (!is_string($config)) {
+                FatalError::invalidArgType(
+                    __METHOD__,
+                    'config',
+                    'string|array');
+            }
+
+            $config
+            = strtolower($config);
+
+            if (!isset(slef::$_config[$config])) {
+                return false;
+            }
+
+            return
+            self::$_config[$config];
+        }
     }
 }
