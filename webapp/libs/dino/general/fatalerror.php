1442cb21cffd5bf9b2c08afc301928efa0455a23
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2023-08-15 22:20:24 +0330

    #FIX FatalError

--- webapp/libs/dino/general/fatalerror.php
+++ webapp/libs/dino/general/fatalerror.php
@@ -10,104 +10,144 @@ namespace Dino\General
         static
         function
         invalidArgType()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         routersNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         routerNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         routerCheckPathNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         routerPathToRouteNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         invalidRouterPath()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         launcherNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         invalidRoute()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         launcherCheckRouteNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         launcherRouteToPathNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         invalidLauncherRoute()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         readOnly()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         invalidMethod()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         pageNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
 
 
         public
         static
         function
         fileNotFound()
-        {}
+        {
+            self::send(__METHOD__);
+        }
+
+
+        public
+        static
+        function
+        send($message)
+        {
+            throw
+            new \Exception($message);
+        }
     }
 }
