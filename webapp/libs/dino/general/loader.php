c7ec10b31378f5478bb5874b15a530ccb0c8b7cd
t-rex-dino <t.rex.dino.7470@gmail.com>
2023-08-15 10:14:14 +0330

    !C@C Loader

--- webapp/libs/dino/general/loader.php
+++ webapp/libs/dino/general/loader.php
@@ -5,5 +5,61 @@
 namespace Dino\General
 {
     class Loader
-    {}
+    {
+        public
+        static
+        function
+        loadClass($className)
+        {
+            if (!is_string($className)) {
+                FatalError::invalidArgType(
+                    __METHOD__,
+                    'className',
+                    'string');
+            }
+
+            if (class_exists($className, false)
+             || interface_exists($className, false)) {
+
+                return;
+            }
+
+            self::loadFile("{$className}.php");
+
+            if (class_exists($className, false)
+             && interface_exists($className, false)) {
+
+                FatalError::classNotFound(
+                    __METHOD__,
+                    $className,
+                    FatalError::CODING_TIME_ERROR);
+            }
+        }
+
+
+        public
+        static
+        function
+        loadFile(
+            $filePath,
+            $once = false)
+        {
+            if (File::check(
+                    $filePath,
+                    $fullPath)) {
+                
+                return
+                $once
+                ? require_once
+                    $fullPath
+                : require
+                    $fullPath;
+            }
+
+            FatalError::FileNotFound(
+                __METHOD__,
+                $filePath,
+                FatalError::STRUCTURE_ERROR);
+        }
+    }
 }
